c7fedcdddb7bb2b6041e2f00e9ccbb0d1f16f47a
myasiny <myasiny@gmail.com>
2018-03-11 16:48:08 +0300

    Data Download, Fill-NaN, Label-Encode

---
+++ get_data.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import urllib.request as request
+from sklearn.preprocessing import LabelEncoder
+
+def download():
+    # Source: https://chronicdata.cdc.gov/500-Cities/500-Cities-City-level-Data-GIS-Friendly-Format-201/dxpw-cm5u
+    request.urlretrieve("https://chronicdata.cdc.gov/api/views/dxpw-cm5u/rows.csv?accessType=DOWNLOAD",
+                        "data/dataset.csv")
+
+    pd.read_csv("data/dataset.csv").to_csv("data/dataset.csv", index=False, columns=["StateAbbr",
+                                                                                     "PlaceName",
+                                                                                     "BPHIGH_CrudePrev",
+                                                                                     "CANCER_CrudePrev",
+                                                                                     "CHD_CrudePrev",
+                                                                                     "CSMOKING_CrudePrev",
+                                                                                     "DIABETES_CrudePrev",
+                                                                                     "HIGHCHOL_CrudePrev",
+                                                                                     "OBESITY_CrudePrev",
+                                                                                     "SLEEP_CrudePrev",
+                                                                                     "TEETHLOST_CrudePrev"])
+
+# def fill_nan():
+#     dataset = pd.read_csv("data/dataset.csv")
+#     cols = ["BPHIGH_CrudePrev",
+#                 "CANCER_CrudePrev",
+#                 "CHD_CrudePrev",
+#                 "CSMOKING_CrudePrev",
+#                 "DIABETES_CrudePrev",
+#                 "HIGHCHOL_CrudePrev",
+#                 "OBESITY_CrudePrev",
+#                 "SLEEP_CrudePrev",
+#                 "TEETHLOST_CrudePrev"]
+#     for col in cols:
+#         dataset[col] = dataset[col].fillna(dataset[col].mean())
+#     dataset.to_csv("data/dataset.csv", index=False)
+
+# def label_encode():
+#     dataset = pd.read_csv("data/dataset.csv")
+#     cols = ["StateAbbr",
+#             "PlaceName"]
+#     for col in cols:
+#         dataset[col] = pd.DataFrame(LabelEncoder().fit_transform(dataset[col]))
+#     dataset.to_csv("data/dataset.csv", index=False)
+
+if __name__ == "__main__":
+    download()
